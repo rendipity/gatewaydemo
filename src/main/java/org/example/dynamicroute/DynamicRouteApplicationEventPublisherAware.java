45bd7177b798a9884b280b0ebf022567af3aa58c
serendipity <1943247190@qq.com>
2023-07-27 08:28:25 +0800

    feat:dynamicroute

---
+++ src/main/java/org/example/dynamicroute/DynamicRouteApplicationEventPublisherAware.java
@@ -0,0 +1,24 @@
+package org.example.dynamicroute;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.ApplicationEventPublisherAware;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+
+@Component
+@Slf4j
+public class DynamicRouteApplicationEventPublisherAware implements ApplicationEventPublisherAware {
+
+    @Resource
+    DynamicRouteService dynamicRouteService;
+
+    ApplicationEventPublisher applicationEventPublisher;
+    @Override
+    public void setApplicationEventPublisher(ApplicationEventPublisher applicationEventPublisher) {
+        log.info("ApplicationEventPublisherAware 执行了");
+        this.applicationEventPublisher = applicationEventPublisher;
+        dynamicRouteService.addRoute();
+    }
+}
--- src/main/java/org/example/dynamicroute/DynamicRouteService.java
+++ src/main/java/org/example/dynamicroute/DynamicRouteService.java
@@ -2,11 +2,25 @@ package org.example.dynamicroute;
 
 
 import cn.hutool.core.util.IdUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.FilterDefinition;
+import org.springframework.cloud.gateway.handler.predicate.PredicateDefinition;
 import org.springframework.cloud.gateway.route.RouteDefinition;
 import org.springframework.cloud.gateway.route.RouteDefinitionWriter;
+import org.springframework.stereotype.Service;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
+import reactor.core.publisher.Mono;
 
 import javax.annotation.Resource;
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
+@Slf4j
+@Service
 public class DynamicRouteService {
 
     @Resource
@@ -16,5 +30,24 @@ public class DynamicRouteService {
         RouteDefinition definition = new RouteDefinition();
         // route id
         definition.setId(IdUtil.simpleUUID());
+        // uri
+        URI uri = UriComponentsBuilder.fromHttpUrl("http://localhost:8001").build().toUri();
+        definition.setUri(uri);
+        // predicate
+        PredicateDefinition predicate = new PredicateDefinition();
+        predicate.setName("Path");
+        Map<String,String> predicateParams = new HashMap<>();
+        predicateParams.put("_genkey_0","/publicApi/**");
+        predicate.setArgs(predicateParams);
+        definition.setPredicates(Stream.of(predicate).collect(Collectors.toList()));
+        // filter
+        FilterDefinition filter = new FilterDefinition();
+        filter.setName("StripPrefix");
+        Map<String,String> filterParam = new HashMap<>();
+        filterParam.put("_genkey_0","1");
+        filter.setArgs(filterParam);
+        definition.setFilters(Stream.of(filter).collect(Collectors.toList()));
+        routeDefinitionWriter.save(Mono.just(definition)).subscribe();
+        log.info("addRouteSuccess");
     }
 }
